2f8ce3906f06779d8e85d2e419cb8ffe461a10f9
alanisg <13020522gA!>
2020-03-12 11:15:33 -0300

    Versão final, pronta para enrtega

--- app/src/main/java/com/example/atv03_pdm/MainActivity.java
+++ app/src/main/java/com/example/atv03_pdm/MainActivity.java
@@ -224,7 +224,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
-        Toast.makeText(this, "Versão final", Toast.LENGTH_SHORT).show();    
+        Toast.makeText(this, "Versão final", Toast.LENGTH_SHORT).show();           
     }
 
 }
